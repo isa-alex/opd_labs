438eefca938f8f91fa513633553b7920bdedd8f0
Alex <124351863+isa-alex@users.noreply.github.com>
2024-11-16 15:47:44 +0300

    Add files via upload

--- scr4.sh
+++ scr4.sh
@@ -6,16 +6,16 @@ wc -m * 2>/dev/null | sort -nr
 
 cd ..
 
-ls -R magby6 2>/dev/null | grep -v ':$' | sort -r
+ls -R magby6 2>&1 | grep -v ':$' | sort -r 
 
 
 grep "do" crobat4/xatu crobat4/rampardos crobat4/doduo crobat4/crobat gallade6/hoppip 2>/tmp/errors_lab1
 
 
-ls -lR lab0 2>/dev/null | grep '^c' | sort -k 6 -r | tail -n 3
+ls -R lab0 2>/dev/null | grep '^c' | sort -k 6 -r | tail -n 3
 
 
-grep -rl "ru" | sort -k 6,7
+grep -rl "ru" 2>&1 | sort -k 6,7
 
 
 ls -lR | sort -k 6 -r | head -n 3
